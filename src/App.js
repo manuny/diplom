fa907f12f4839bc0b12c5b8b068aea51c75458a9
Vasileva Masha <Vasilevamasha1982@gmail.com>
2024-06-16 02:11:21 +0300

    начало

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./img/logo.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header className="header">
+        <a href="#" className="logo">
+          <img src={logo} alt="logo" />
         </a>
+        <nav>
+          <a href="#">Главная</a>
+          <a href="#">Новости</a>
+          <a href="#">Педагоги</a>
+          <a href="#">Галерея</a>
+          <a href="#">Контакты</a>
+        </nav>
       </header>
+      <div className="content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+        </p>
+      </div>
+      <footer className="footer"></footer>
     </div>
   );
 }
