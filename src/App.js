a0e2449fc46a677088cd7eee4ebf1718d341d111
Vasileva Masha <Vasilevamasha1982@gmail.com>
2024-06-17 04:34:35 +0300

    новости

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
+import Novosti from "./components/Novosti/Novosti";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Content />
+      <Novosti />
+
       <Footer />
     </div>
   );
